77367ccf3650a224461bec06304aaae4c3a5d7a2
Md Mumin Khan <mdmuminkhan84@gmail.com>
2024-09-20 22:31:29 +0600

    Draz_WebView.Xml

---
+++ app/src/main/java/com/example/web_view2/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.web_view2
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isGone
+import com.example.web_view2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.webView.loadUrl("https://www.daraz.com.bd/#hp-flash-sale")
+        binding.webView.settings.javaScriptEnabled = true
+
+        binding.webView.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+
+                binding.webView.visibility = View.GONE
+
+                binding.bar.visibility = View.VISIBLE
+
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                binding.webView.visibility = View.VISIBLE
+                binding.bar.visibility = View.GONE
+            }
+
+        }
+
+    }
+
+    override fun onBackPressed() {
+        if (binding.webView.canGoBack()) {
+            binding.webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
